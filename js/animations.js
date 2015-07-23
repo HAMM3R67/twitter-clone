ea8a95a964ba5f97d54020412ff49b23f89a4ddc
HAMM3R67 <tcschauer@gmail.com>
2015-07-22 20:37:34 -0600

    added on click animation to double in size

--- js/animations.js
+++ js/animations.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
 	$("#char-count").hide();
 	
 	$(".button").hide();
+	
+	$(".tweet-compose").on('click', function(){
+		$(".tweet-compose").animate({height: '5em'}, 500);
+	})
 
 	
 	
